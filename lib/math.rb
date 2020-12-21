6e268243c70fac7cc37c62be2869a4d9f5a24e8b
C. L <amautacelt@gmail.com>
2020-12-21 06:49:40 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -7,15 +7,15 @@ def subtraction(num1, num2)
 end
 
 def division(num1, num2)
-  20 / 4
+  num1 / num2
 end
 
 def multiplication(num1, num2)
-  5 * 4
+  num1 * num2
 end
 
 def modulo(num1, num2)
-  5 % 4
+  num1 % num2
 end
 
 def square_root(num)
